dfabe7d6174e4e8617d09bc99dc5c3d8d1c46624
Paul Salaets <psalaets@gmail.com>
2014-12-04 19:34:48 -0500

    add ng route as dep to app module

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 var angular = require('angular');
 
-var app = angular.module('app', []);
+var app = angular.module('app', ['ngRoute']);
 
 app.controller('IndexCtrl', function($scope) {
   $scope.message = 'hello';
