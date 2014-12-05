be17f85132f361ff939bc06a77a76e49e51b1793
Paul Salaets <psalaets@gmail.com>
2014-12-04 19:48:40 -0500

    hello world with ngRoute in there

--- app/index.js
+++ app/index.js
@@ -2,6 +2,15 @@ var angular = require('angular');
 
 var app = angular.module('app', ['ngRoute']);
 
-app.controller('IndexCtrl', function($scope) {
-  $scope.message = 'hello';
+app.config(function appConfig($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'templates/title.html',
+      controller: 'IndexCtrl',
+      controllerAs: 'main'
+    });
+});
+
+app.controller('IndexCtrl', function() {
+  this.message = 'hello';
 });
