60d126f85eb34135d1ca514e3aab78e6c7c9354b
Paul Salaets <psalaets@gmail.com>
2014-12-05 20:50:47 -0500

    started title route and new game route

--- app/index.js
+++ app/index.js
@@ -6,11 +6,37 @@ app.config(function appConfig($routeProvider) {
   $routeProvider
     .when('/', {
       templateUrl: 'templates/title.html',
-      controller: 'IndexCtrl',
-      controllerAs: 'main'
+      controller: 'TitleCtrl',
+      controllerAs: 'shang'
+    })
+    .when('/games/new', {
+      templateUrl: 'templates/game/new.html',
+      controller: 'NewGameCtrl',
+      controllerAs: 'setup'
     });
 });
 
-app.controller('IndexCtrl', function() {
-  this.message = 'hello';
+app.controller('TitleCtrl', function($location) {
+  this.newGame = function() {
+    $location.path('/games/new')
+  };
+});
+
+
+app.controller('NewGameCtrl', function() {
+  this.playerName = '';
+
+  this.players = [];
+
+  this.add = function() {
+    this.players.push(this.playerName);
+    this.playerName = '';
+  };
+
+  this.remove = function(player) {
+    var index = this.players.indexOf(player);
+    if (index != -1) {
+      this.players.splice(index, 1);
+    }
+  };
 });
