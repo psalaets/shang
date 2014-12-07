6f0c60ed57d2c3046e7815e388bc5204ac4ef11f
Paul Salaets <psalaets@gmail.com>
2014-12-06 22:09:41 -0500

    empty shell for game test

--- test/first.js
+++
@@ -1,7 +0,0 @@
-var assert = chai.assert;
-
-describe('first', function () {
-  it('should blah', function () {
-    assert.ok(true);
-  });
-});
---
+++ test/model/game-test.js
@@ -0,0 +1,7 @@
+var assert = chai.assert;
+
+describe('Game', function () {
+  it('should', function () {
+
+  });
+});
