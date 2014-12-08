ef763f95d62007d7c222399cb96b429e5df8b03f
Paul Salaets <psalaets@gmail.com>
2014-12-08 17:29:52 -0500

    rearrange

--- app/services/current-game.service.js
+++ app/services/current-game.service.js
@@ -1,16 +1,16 @@
 ;(function(angular) {
-  var currentGame = {
-    game: null,
-    get: function() {
-      return this.game;
-    },
-    set: function(game) {
-      this.game = game;
-    }
-  };
-
   angular.module('app.services')
-    .factory('currentGame', function() {
-      return currentGame;
-    });
+    .factory('currentGame', currentGame);
+
+  function currentGame() {
+    return {
+      game: null,
+      get: function() {
+        return this.game;
+      },
+      set: function(game) {
+        this.game = game;
+      }
+    };
+  }
 })(angular);
