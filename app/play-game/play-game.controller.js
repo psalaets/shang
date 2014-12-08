7b55558c702c890e881bc5eab5cf0ba39bf5e970
Paul Salaets <psalaets@gmail.com>
2014-12-08 18:08:06 -0500

    forgot to add this

---
+++ app/play-game/play-game.controller.js
@@ -0,0 +1,8 @@
+;(function(angular) {
+  angular.module('app.play-game')
+    .controller('PlayGameController', PlayGameController);
+
+  function PlayGameController() {
+
+  }
+})(angular);
---
+++ app/play-game/play-game.module.js
@@ -0,0 +1,1 @@
+angular.module('app.play-game', []);
