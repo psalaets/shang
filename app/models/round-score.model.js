85245db594b7d64d97f8029fe2f73d7ccb702d6e
Paul Salaets <psalaets@gmail.com>
2014-12-08 22:35:41 -0500

    hold off on the rawScore/actualScore idea and score modifier flags because yagni

--- app/models/round-score.model.js
+++ app/models/round-score.model.js
@@ -3,27 +3,6 @@
 
   function RoundScore(player) {
     this.player = player;
-    this.rawScore = null;
-    this.perfectDeal = false;
-    this.shanghai = false;
+    this.score = null;
   }
-
-  var p = RoundScore.prototype;
-
-  Object.defineProperty(p, 'actualScore', {
-    get: function() {
-      var actualScore = this.rawScore || 0;
-
-      if (this.perfectDeal) {
-        actualScore -= 5;
-      }
-
-      if (this.shanghai && actualScore > 0) {
-        actualScore *= 2;
-      }
-
-      return actualScore;
-    }
-  });
-
 })(angular);
--- test/models/round-score.model.test.js
+++ test/models/round-score.model.test.js
@@ -15,91 +15,10 @@ describe('RoundScore', function () {
       assert.equal(roundScore.player, 'emily');
     });
 
-    it('starts raw score as null', function () {
+    it('starts score as null', function () {
       var roundScore = new RoundScore('billy');
 
-      assert.equal(roundScore.rawScore, null);
-    });
-
-    it('starts perfect deal as false', function () {
-      var roundScore = new RoundScore('billy');
-
-      assert.equal(roundScore.perfectDeal, false);
-    });
-
-    it('starts shanghai as false', function () {
-      var roundScore = new RoundScore('billy');
-
-      assert.equal(roundScore.shanghai, false);
-    });
-  });
-
-  describe('#actualScore', function () {
-    describe('rawScore has never been set', function () {
-      it('is 0', function() {
-        var roundScore = new RoundScore('billy');
-
-        assert.equal(roundScore.actualScore, 0);
-      });
-    });
-
-    describe('rawScore is > 0', function () {
-      it('is same as raw score', function() {
-        var roundScore = new RoundScore('billy');
-
-        roundScore.rawScore = 100;
-
-        assert.equal(roundScore.actualScore, 100);
-      });
-
-      it('is 5 less than raw score when perfect deal is true', function() {
-        var roundScore = new RoundScore('billy');
-
-        roundScore.rawScore = 100;
-        roundScore.perfectDeal = true;
-
-        assert.equal(roundScore.actualScore, 95);
-      });
-
-      it('is 2x raw score when shanghai is true', function() {
-        var roundScore = new RoundScore('billy');
-
-        roundScore.rawScore = 100;
-        roundScore.shanghai = true;
-
-        assert.equal(roundScore.actualScore, 200);
-      });
-
-      it('is 2x of 5 less than raw score when shanghai and perfect deal are true', function() {
-        var roundScore = new RoundScore('billy');
-
-        roundScore.rawScore = 100;
-        roundScore.shanghai = true;
-        roundScore.perfectDeal = true;
-
-        assert.equal(roundScore.actualScore, 190);
-      });
-    });
-
-    describe('rawScore is 0', function () {
-      it('is same as rawScore, when shanghai is true', function() {
-        var roundScore = new RoundScore('billy');
-
-        roundScore.rawScore = 0;
-        roundScore.shanghai = true;
-
-        assert.equal(roundScore.actualScore, 0);
-      });
-
-      it('is 5 less than rawScore, when shanghai and perfect deal are true', function() {
-        var roundScore = new RoundScore('billy');
-
-        roundScore.rawScore = 0;
-        roundScore.shanghai = true;
-        roundScore.perfectDeal = true;
-
-        assert.equal(roundScore.actualScore, -5);
-      });
+      assert.equal(roundScore.score, null);
     });
   });
 });
