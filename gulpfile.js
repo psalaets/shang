d8c35e0de256439e39e62e5a2c7e493f920b48cb
Paul Salaets <psalaets@gmail.com>
2014-12-17 13:58:55 -0500

    starting on gulp tasks for building deployable app

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,28 @@
 var gulp = require('gulp'),
-    browserSync = require('browser-sync');
+    browserSync = require('browser-sync'),
+    useref = require('gulp-useref'),
+    filter = require('gulp-filter'),
+    filelog = require('gulp-filelog'),
+    ngAnnotate = require('gulp-ng-annotate'),
+    gulpif = require('gulp-if'),
+    uglify = require('gulp-uglify'),
+    rev = require('gulp-rev')
+
+gulp.task('prep-js', function() {
+  return gulp.src('app/index.html')
+    // concat files between build/endbuild comments into a file named according
+    // to build block params
+    .pipe(useref.assets())
+    // filter down to just js files (i.e. no css)
+    .pipe(filter('*.js'))
+    // make our angular code minifiable
+    .pipe(gulpif(/app.js/, ngAnnotate()))
+    // minify
+    .pipe(uglify())
+    // give unique filename based on file contents
+    .pipe(rev())
+    .pipe(gulp.dest('build/scripts'))
+});
 
 gulp.task('watch', function(cb) {
   browserSync({
