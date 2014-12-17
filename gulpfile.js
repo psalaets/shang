73592aea318937293ff1672476eea5b787e19d3e
Paul Salaets <psalaets@gmail.com>
2014-12-17 13:26:05 -0500

    put callback in right spot

--- gulpfile.js
+++ gulpfile.js
@@ -7,17 +7,17 @@ gulp.task('watch', function(cb) {
       baseDir: './app/',
       // don't mirror clicks, scroll, forms across browsers
       ghostMode: false
-    },
-    function() {
-      gulp.watch([
-        'app/**/*.html',
-        'app/**/*.js',
-        'app/**/*.css'
-      ], {}, browserSync.reload);
-
-      // signal to gulp that this task is done
-      cb();
     }
+  },
+  function() {
+    gulp.watch([
+      'app/**/*.html',
+      'app/**/*.js',
+      'app/**/*.css'
+    ], {}, browserSync.reload);
+
+    // signal to gulp that this task is done
+    cb();
   });
 });
 
