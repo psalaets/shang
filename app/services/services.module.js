435cef230b51280ffe52c694b669e713647764f2
Paul Salaets <psalaets@gmail.com>
2015-02-27 16:34:42 -0500

    namespace the lf data

--- app/services/services.module.js
+++ app/services/services.module.js
@@ -2,4 +2,9 @@ angular.module('app.services', [
   'app.models',
   // offline storage
   'LocalForageModule'
-]);
+]).config(function($localForageProvider) {
+  // namespace the underlying localforage instance
+  $localForageProvider.config({
+    name: 'Shanghai Rummy'
+  });
+});
