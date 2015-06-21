2234d01a6c2b056cc74cc683d33e4e9d955ede71
Paul Salaets <psalaets@gmail.com>
2015-06-20 22:42:24 -0400

    ok angular doesn't let you do this (anymore?)

--- app/config.module.js
+++ app/config.module.js
@@ -3,8 +3,4 @@
 * module. That's the only purpose this module serves.
 */
 angular.module('app.config', [])
-  // Hack: since angular let's things be defined twice this is the config for
-  // dev. Now everywhere that 'config' is used we have to provide defaults since
-  // config could be empty. Doing this because there isn't a build step in dev,
-  // yet.
   .constant('config', {});
